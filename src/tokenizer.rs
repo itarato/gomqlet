ca67e42e6c218158c8e05a75f412a216d108716d
Peter Arato <it.arato@gmail.com>
2024-12-12 20:35:12 -0500

    whitespace

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -26,9 +26,10 @@ impl Tokenizer {
                     source_iter.next().expect("Bad iteration");
                 }
                 'a'..='z' => {
-                    let (token, keyword_len) = Tokenizer::consume_keyword(source_iter.clone());
-                    tokens.push(token);
-                    source_iter.skip(keyword_len);
+                    tokens.push(Tokenizer::consume_keyword(&mut source_iter));
+                }
+                ' ' | '\t' | '\r' | '\n' => {
+                    source_iter.next().expect("Bad iteration");
                 }
                 _ => unimplemented!(),
             }
@@ -37,13 +38,19 @@ impl Tokenizer {
         tokens
     }
 
-    fn consume_keyword(source_iter: Peekable<Chars<'_>>) -> (Token, usize) {
-        let fragment = source_iter
-            .take_while(|ch| ch.is_ascii_alphabetic())
-            .collect::<String>();
+    fn consume_keyword(source_iter: &mut Peekable<Chars<'_>>) -> Token {
+        let mut fragment = String::new();
 
-        let len = fragment.len();
-        (Token::Keyword(fragment), len)
+        while let Some(ch) = source_iter.peek() {
+            if !ch.is_ascii_alphabetic() {
+                break;
+            }
+
+            fragment.push(*ch);
+            source_iter.next().unwrap();
+        }
+
+        Token::Keyword(fragment)
     }
 }
 
@@ -69,7 +76,17 @@ mod test {
 
     #[test]
     fn test_keyword() {
-        let tokens = Tokenizer::tokenize("{user}{}");
+        let tokens = Tokenizer::tokenize("{user}");
+        dbg!(&tokens);
+        assert_eq!(3, tokens.len());
+        assert_eq!(Token::OpenBrace, tokens[0]);
+        assert_eq!(Token::Keyword("user".into()), tokens[1]);
+        assert_eq!(Token::CloseBrace, tokens[2]);
+    }
+
+    #[test]
+    fn test_keyword_with_whitespaces() {
+        let tokens = Tokenizer::tokenize("\t {     \n\nuser\r\n  }    ");
         dbg!(&tokens);
         assert_eq!(3, tokens.len());
         assert_eq!(Token::OpenBrace, tokens[0]);
