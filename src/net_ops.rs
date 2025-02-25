54d1ff70585b1ba920255a879275439eb7cefe9c
Peter Arato <it.arato@gmail.com>
2025-02-25 14:13:24 -0500

    Increase timeout.

--- src/net_ops.rs
+++ src/net_ops.rs
@@ -11,7 +11,7 @@ use crate::{
 };
 
 const INSPECTION_QUERY: &'static str = "query IntrospectionQuery { __schema { queryType { name } mutationType { name } subscriptionType { name } types { ...FullType } directives { name description locations args { ...InputValue } } }}fragment FullType on __Type { kind name description fields(includeDeprecated: true) { name description args { ...InputValue } type { ...TypeRef } isDeprecated deprecationReason } inputFields { ...InputValue } interfaces { ...TypeRef } enumValues(includeDeprecated: true) { name description isDeprecated deprecationReason } possibleTypes { ...TypeRef }}fragment InputValue on __InputValue { name description type { ...TypeRef } defaultValue}fragment TypeRef on __Type { kind name ofType { kind name ofType { kind name ofType { kind name ofType { kind name ofType { kind name } } } } }}";
-const TIMEOUT_SECONDS: u64 = 120;
+const TIMEOUT_SECONDS: u64 = 360;
 
 pub struct NetOps {
     client: reqwest::blocking::Client,
